4c88f0a3b43e8aec58cd93f6142cc264a8fc22ea
Luckinfort8 <krodriguezt2002@alumno.ipn.mx>
2022-10-23 22:14:24 -0500

    commit ejemplos

---
+++ Javascript/ejemplo3/scripts/script.js
@@ -0,0 +1,9 @@
+function abrirRegalo() {
+  const image = document.querySelector('img');
+  image.src = '../../images/giphy.gif';
+  image.removeEventListener('click', abrirRegalo);
+}
+
+const image = document.querySelector('img');
+
+image.addEventListener('click', abrirRegalo);
